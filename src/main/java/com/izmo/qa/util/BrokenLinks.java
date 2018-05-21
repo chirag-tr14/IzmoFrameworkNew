7a3f109025dd3ec6ab24257a6947e5a4cc5c5836
rajesh.n <rajesh.n@IZDT-211.networksrv.com>
2018-05-21 18:58:35 +0530

    Signed-off-by: rajesh.n <rajesh.n@IZDT-211.networksrv.com>

---
+++ src/main/java/com/izmo/qa/util/BrokenLinks.java
@@ -0,0 +1,35 @@
+package com.izmo.qa.util;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class BrokenLinks {
+
+
+	
+	public static void verifyLinkActive(String linkUrl)
+	{
+        try 
+        {
+           URL url = new URL(linkUrl);
+           
+           HttpURLConnection httpURLConnect=(HttpURLConnection)url.openConnection();
+           
+           httpURLConnect.setConnectTimeout(3000);
+           
+           httpURLConnect.connect();
+           
+           if(httpURLConnect.getResponseCode()>=400)
+           {
+               System.out.println(linkUrl+" - "+httpURLConnect.getResponseMessage());
+            }
+          if(httpURLConnect.getResponseCode()==HttpURLConnection.HTTP_NOT_FOUND)  
+           {
+               System.out.println(linkUrl+" - "+httpURLConnect.getResponseMessage() + " - "+ HttpURLConnection.HTTP_NOT_FOUND);
+            }
+        } catch (Exception e) {
+           
+        }
+    } 
+}	
+	
